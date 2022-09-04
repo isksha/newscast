3f92c0e248ed78f08de68c0163e073d136dd2558
emreguler <eguler@sas.upenn.edu>
2022-09-04 06:08:01 -0400

    finalized changes

--- generate_transcript.py
+++ generate_transcript.py
@@ -1,16 +1,21 @@
 # Text-to-speech API from https://cloud.google.com/text-to-speech/
-
+from datetime import datetime
 import openai
+import json
+
 import retrieve_from_gmail
 from google.cloud import texttospeech
 
 openai.api_key = "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"
 newsletters = retrieve_from_gmail.main()
-transcript = [(0, "Good morning, here is your morning briefing, powered by Newscast.")]
-fulltext = ''
+article_count = 0
+
+fulltext = "Good morning! Here is your daily briefing, curated from your newsletters. "
+fulltext += 'Today is ' + datetime.today().strftime('%A, %B %d.') + '\n'
+
 
 def generate_summarize_prompt(text):
-    return """Summarize the following newsletter in three to four sentences in the third person:
+    return """Summarize the following newsletter in three to four sentences in the third person and with a conversational tone:
     "{}" """.format(text)
 
 
@@ -24,48 +29,52 @@ def summarize(text):
     return reply['choices'][0]['text'][2:]
 
 
-def up_next_segment():
-    for idx, elt in enumerate(newsletters):
-        transcript.append((idx + 1, summarize(elt[2])))
+def summarizing_segment(i):
+    global fulltext
+    fulltext += summarize(newsletters[i][2])
 
 
-up_next_segment()
-transcript.append((0, "Buh bye"))
+for i in range(len(newsletters)):
+    summarizing_segment(i)
+    fulltext += "\n"
 
+fulltext += "That's everything for today; thanks for tuning into Newscast!"
 
-def generate_string():
-    for i in transcript:
-        global fulltext
-        fulltext += i[1] + '\n'
+def text_to_speech(fulltext):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
 
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=fulltext)
 
-generate_string()
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
 
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
 
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
+    # The response's audio_content is binary.
+    with open("newscast/src/output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(text=fulltext)
+def export_json():
+    dictionary = {
+        "text": fulltext
+    }
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.VoiceSelectionParams(
-language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-)
+    json_object = json.dumps(dictionary)
+    with open("newscast/src/fulltext.json", "w") as outfile:
+        outfile.write(json_object)
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
-
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
-
-# The response's audio_content is binary.
-with open("newscast/src/output.mp3", "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+text_to_speech(fulltext)
+export_json()
--- newscast/src/App.js
+++ newscast/src/App.js
@@ -7,50 +7,46 @@ import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import sound from './output.mp3'
+import text from './fulltext.json'
 
-const audio_src = 'output.mp3';
-
-const transcript = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar placerat magna sit amet posuere. Curabitur interdum, nibh id hendrerit dictum, risus eros pellentesque nibh, in fermentum nunc libero ut justo. In hac habitasse platea dictumst. Nunc id lacus ullamcorper, blandit libero nec, dignissim turpis. Nunc placerat nisi quis quam maximus varius. Duis ullamcorper eu ex ac congue. Nam eu scelerisque tellus, vitae vehicula turpis. Sed vitae varius magna, in convallis ligula. Mauris rhoncus massa lorem, vel tempus quam luctus vitae.\n
-            
-Donec quis lacinia augue. Nam at purus a nisi luctus dignissim in non tellus. Curabitur a erat ut dolor maximus posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer accumsan dui tellus, ut ultrices libero molestie at. In dui massa, mattis at leo nec, porttitor pharetra elit. Maecenas eget lorem tincidunt, sodales sem eget, egestas felis. In vestibulum finibus odio, vitae venenatis lectus dapibus eu. Aenean pharetra dolor nec gravida dictum.\n
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut volutpat lorem. Suspendisse gravida, sem vel gravida condimentum, sapien odio lobortis massa, sagittis fermentum neque nisi et odio. Maecenas eros felis, rhoncus in lectus ut, interdum tincidunt massa. In nibh tellus, placerat placerat velit vel, suscipit pharetra ex. Aenean quis lorem arcu. Aliquam a gravida erat, et placerat massa. Donec interdum tincidunt nunc, vel luctus odio fringilla nec.\n
-Nulla ut leo eu urna ullamcorper ullamcorper non sit amet nibh. Nunc semper erat sit amet nisi tincidunt, id maximus nunc consequat. Morbi a augue justo. In dui nulla, finibus ac odio et, pellentesque maximus odio. Praesent at mi nec nisi hendrerit pretium pulvinar nec nisl. Nulla felis mi, euismod eget porta eu, vestibulum sit amet arcu. Maecenas vehicula auctor mollis. Vivamus quis sem sed risus hendrerit dignissim nec ut eros. Maecenas vel elementum tellus. Aliquam lorem justo, auctor vel pharetra at, maximus ut erat.\n
-Sed suscipit justo lobortis volutpat auctor. Morbi rutrum est libero, eget congue libero fermentum lacinia. Donec ac ullamcorper tellus, sed vulputate ante. Aenean ut aliquam lorem. Sed vitae eros et lectus efficitur tristique. Integer ut gravida quam. Phasellus ornare metus leo, sed rutrum lectus luctus sit amet. Nam porta eros ut massa efficitur euismod.`
+const transcript = text.text
 
 export default function App() {
-  return (
-      <div className = "App">
-        <header className = "App-header">
-          <Typography variant = 'h2' component = 'p'>Your daily newsletter brief, automated!</Typography>
-          <Box sx={{ width: '99%', position: 'fixed', top: 100}}>
-            <AudioPlayer
-                preload="auto"
-                src={audio_src}
-            />
-          </Box>
-          <Box sx={{ width: '99%', position: 'fixed', top: 230, mb: 2 }}>
-            <Accordion>
-              <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-              >
-                <Typography>See the transcript!</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography id="modal-description"
-                            sx={{ whiteSpace: 'pre-line', mb: 2,
-                              display: "flex",
-                              flexDirection: "column",
-                              height: 500,
-                              overflow: "hidden",
-                              overflowY: "scroll"}}>
-                  {transcript}
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          </Box>
-        </header>
-      </div>
-  );
+    return (
+        <div className = "App">
+            <header className = "App-header">
+                <Typography variant = 'h2' component = 'p'>Your daily newsletter brief, automated!</Typography>
+                <Box sx={{ width: '99%', position: 'fixed', top: 100}}>
+                    <AudioPlayer
+                        preload="auto"
+                        src={sound}
+                        muted="true"
+                    />
+                </Box>
+                <Box sx={{ width: '99%', position: 'fixed', left: 5, top: 230, mb: 2 }}>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>See the transcript!</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography id="modal-description"
+                                        sx={{ whiteSpace: 'pre-line', mb: 2,
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            height: 500,
+                                            overflow: "hidden",
+                                            overflowY: "scroll"}}>
+                                {transcript}
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </Box>
+            </header>
+        </div>
+    );
 }
