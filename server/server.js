a08b85dce4097ce1b88de6c05241f4f9a353f379
Iskander Shangareev <isksha@seas.upenn.edu>
2023-05-26 12:46:39 +0200

    finished transcript CRUD ops

--- server/server.js
+++ server/server.js
@@ -18,9 +18,12 @@ const dbLib = require('./dbOperations');
 webapp.get('/', async (req, res) => {
   dbLib.connect();
   // const newsletterStr = await transcriptExtracter.getTranscript('Hello', 'World');
-  // dbLib.addNewscast('iskander', 'general', newsletterStr);
-  const resp = await dbLib.getNewscast('iskander', 'general', new Date('2023-05-26'));
-  res.json({ message: "What's up" });
+  // dbLib.addNewscast('iskander', 'general', newsletterStr, new Date()));
+  // const resp = await dbLib.getNewscast('iskander', 'general', new Date('2023-05-26'));
+  // const resp = await dbLib.deleteNewscast('iskander', 'sports', new Date('2023-05-26'));
+  // const resp = await dbLib.getNewscastsByUserAndTopic('iskander', 'sports');
+  // const resp = await dbLib.updateNewscast('iskander', 'sports', new Date('2023-05-26'), 'The thing about Arsenal is they always try and walk it in');
+  res.json({ message: 'hello' });
 });
 
 // export the webapp
