2713f1650e3beb28acb168fc120ff97ceeb590df
Iskander Shangareev <isksha@seas.upenn.edu>
2023-07-09 20:46:51 +0200

    began refactoring python script

--- server/api/generate_transcript.py
+++ server/api/generate_transcript.py
@@ -1,17 +1,103 @@
-# Text-to-speech API from https://cloud.google.com/text-to-speech/
-from datetime import datetime
-import openai
-import json
-
-import retrieve_from_gmail
-# from google.cloud import texttospeech
+from __future__ import print_function
 from dotenv import dotenv_values
+import openai
+from datetime import datetime
+from datetime import date
+from bs4 import BeautifulSoup
+import os.path
+import base64
+import os
+import sys
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def fetch_emails(start, end):
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    # """
+
+    # suppress output from gmail API
+    org_stdout = sys.stdout
+    f = open(os.devnull, 'w')
+    sys.stdout = f
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('../utils/token.json'):
+        creds = Credentials.from_authorized_user_file(
+            '../utils/token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../utils/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('../utils/token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+        query = '"listid" OR "newsletter" AND after:{}'.format(
+            date.today().strftime("%Y/%m/%d"))
+        newsletter_ids = service.users().messages().list(
+            userId='me', q=query).execute().get('messages', [])
+
+        # Create list of tuples for each newsletter.
+        newsletters = []
+
+        # Add the 3-tuple to the list. [0] is sender, [1] is subject, [2] is full text.
+        for my_id in newsletter_ids:
+            newsletter_full = service.users().messages().get(
+                userId='me', id=my_id['id'], format='full').execute()
+            payload = newsletter_full['payload']
+
+            # Retrieve sender and subject
+            sender = ''
+            subject = ''
+            for header in payload['headers']:
+                if header['name'] == 'From':
+                    sender = header['value'].rsplit(' ', 1)[0]
+                elif header['name'] == 'Subject':
+                    subject = header['value']
+
+            # Retrieve body text of newsletter
+            data = payload['parts'][1]['body']['data']
+            html_body = base64.urlsafe_b64decode(data).decode('utf-8')
+            html_body = html_body.replace('\u200c', '')
+            html_body = html_body.replace('\xa0', ' ')
+            normalized_html_body = ' '.join(html_body.split())
+            soup = BeautifulSoup(normalized_html_body, 'html.parser')
+            full_text = soup.get_text()
+            full_text = full_text.replace(f"{chr(32)}{chr(32)}", '')
+
+            # Add the 3-tuple to the list. [0] is sender, [1] is subject, [2] is full text.
+            newsletter = (sender, subject, full_text)
+            newsletters.append(newsletter)
+        sys.stdout = org_stdout
+        return newsletters
+
+    except HttpError as error:
+        return None
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 config = dotenv_values("../.env")
 openai.api_key = config['OPENAI_API_KEY']
 
-newsletters = retrieve_from_gmail.main()[:5]
-article_count = 0
+newsletters = fetch_emails()[:5]
 
 fulltext = "Good morning! Here is your daily briefing, curated from your newsletters. "
 fulltext += 'Today is ' + datetime.today().strftime('%A, %B %d.') + '\n'
@@ -57,46 +143,4 @@ else:
         summarizing_segment(i)
     fulltext += "That's everything for today; thanks for tuning into Newscast!"
 
-
-def text_to_speech(fulltext):
-    # Instantiates a client
-    client = texttospeech.TextToSpeechClient()
-
-    # Set the text input to be synthesized
-    synthesis_input = texttospeech.SynthesisInput(text=fulltext)
-
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-    )
-
-    # Select the type of audio file you want returned
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3)
-
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
-    response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config
-    )
-
-    # The response's audio_content is binary.
-    with open("newscast/src/output.mp3", "wb") as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-
-
-def export_json():
-    dictionary = {
-        "text": fulltext
-    }
-
-    json_object = json.dumps(dictionary)
-    with open("newscast/src/fulltext.json", "w") as outfile:
-        outfile.write(json_object)
-
-
 print(fulltext)
-# text_to_speech(fulltext)
-# export_json()
